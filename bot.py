50e475559cb1f38f84f447270283899e8ccb4818
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2023-08-18 23:43:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -328,23 +328,23 @@ def handle_token_time_command(client, message):
         
 # Handler for the /delete_userid command (accessible by admin only)
 @app.on_message(filters.command("delete_userid") & filters.user(admin_id))
-def handle_delete_userid_command(client, message):
+def handle_delete_file_data_command(client, message):
     try:
         if len(message.command) < 2:
             app.send_message(
-                message.chat.id, "Please provide a user ID to delete.")
+                message.chat.id, "Please provide a file caption to delete.")
             return
 
-        user_id_to_delete = int(message.command[1])
+        file_caption_to_delete = " ".join(message.command[1:])
 
-        # Delete the user from the database
-        delete_user_data(user_id_to_delete)
+        # Delete the file data from the database
+        delete_file_data(file_caption_to_delete)
 
         app.send_message(
-            message.chat.id, f"User with ID {user_id_to_delete} has been deleted from the database.")
+            message.chat.id, f"File data with caption '{file_caption_to_delete}' has been deleted from the database.")
     except Exception as e:
         app.send_message(
-            message.chat.id, f"Error while deleting user: {e}")
+            message.chat.id, f"Error while deleting file data: {e}")
 
 # Handler for the /delete_file_id command (accessible by admin only)
 @app.on_message(filters.command("delete_file") & filters.user(admin_id))
