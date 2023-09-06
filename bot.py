28349920c48c411555c00f9b70c5c60c4a9e3213
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2023-09-07 00:14:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ file_ids = {}
 
 
 # Telegram bot credentials
-api_id = int(os.environ.get('API_ID'))
+api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
 
